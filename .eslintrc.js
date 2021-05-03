30b6722e0168e783a89e82920e5831ea4f2ff250
Sergey Slipchenko <me@faergeek.online>
2021-05-03 11:00:18 +0300

    chore(eslint): use config differently

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,9 @@
 module.exports = {
-  reportUnusedDisableDirectives: true,
-  extends: ['eslint:recommended', 'prettier'],
-  parserOptions: { sourceType: 'module' },
-  env: { es2021: true },
-  rules: {
-    'dot-notation': 'warn',
-    eqeqeq: ['warn', 'smart'],
-    'func-names': ['warn', 'as-needed'],
-    'func-style': ['warn', 'declaration', { allowArrowFunctions: true }],
-    'no-alert': 'warn',
-    'no-console': 'warn',
-    'no-throw-literal': 'warn',
-    'no-useless-computed-key': 'warn',
-    'no-useless-concat': 'warn',
-    'no-useless-rename': 'warn',
-    'object-shorthand': 'warn',
-    'operator-assignment': 'warn',
-    'prefer-const': 'warn',
-    'prefer-template': 'warn',
-    radix: 'warn',
-    'spaced-comment': ['warn', 'always', { block: { balanced: true } }],
-    strict: ['warn', 'never'],
-  },
+  extends: ['.'],
+  overrides: [
+    {
+      files: '*',
+      env: { node: true },
+    },
+  ],
 };
