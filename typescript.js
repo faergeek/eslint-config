c48d4e934acd82bd428ccddae4fcc1c838949221
Sergey Slipchenko <me@faergeek.online>
2021-05-24 08:49:33 +0300

    fix: disable @typescript-eslint/explicit-module-boundary-types

--- typescript.js
+++ typescript.js
@@ -9,6 +9,7 @@ module.exports = {
       'warn',
       { 'ts-ignore': 'allow-with-description' },
     ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-shadow': 'warn',
     '@typescript-eslint/no-unused-vars': 'off',
   },
