67f7e4f644176f201391bb4f2c85e5e369101cdb
Sergey Slipchenko <me@faergeek.online>
2021-06-01 22:23:49 +0300

    feat: add eslint configs for automatic runtime
    BREAKING CHANGE: if you use react or preact config, to preserve old
    behavior you need to add -classic suffix to config name, e.g.
    `@faergeek/eslint-config/react-classic` and
    `@faergeek/eslint-config/preact-classic` respectively

---
+++ preact-classic.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['./react-classic'],
+  settings: {
+    react: {
+      pragma: 'h',
+      version: '17',
+    },
+  },
+};
--- react.js
+++ react.js
@@ -1,23 +1,7 @@
 module.exports = {
-  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
-  env: { 'shared-node-browser': true },
-  settings: { react: { version: 'detect' } },
+  extends: ['./react-classic'],
   rules: {
-    'react/display-name': 'off',
-    'react/jsx-boolean-value': 'warn',
-    'react/jsx-curly-brace-presence': 'warn',
-    'react/jsx-fragments': 'warn',
-    'react/jsx-key': [
-      'warn',
-      { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true },
-    ],
-    'react/no-danger': 'warn',
-    'react/jsx-no-target-blank': [
-      'error',
-      { allowReferrer: true, warnOnSpreadAttributes: true },
-    ],
-    'react/no-unused-prop-types': 'warn',
-    'react/prop-types': 'off',
-    'react/require-render-return': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 };
