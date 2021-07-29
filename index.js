16332298ecd2fc47f5ae58558107bee566e61a20
Sergey Slipchenko <me@faergeek.online>
2021-07-29 16:21:53 +0300

    fix: extract jest config into a separate file
    BREAKING CHANGE: jest config is not enabled by default anymore

--- index.js
+++ index.js
@@ -26,21 +26,4 @@ module.exports = {
     'simple-import-sort/imports': 'warn',
     'simple-import-sort/exports': 'warn',
   },
-  overrides: [
-    {
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
-      rules: {
-        'jest/expect-expect': [
-          'warn',
-          { assertFunctionNames: ['expect', 'expectTypeOf'] },
-        ],
-        'jest/no-test-return-statement': 'warn',
-        'jest/prefer-called-with': 'warn',
-        'jest/prefer-spy-on': 'warn',
-        'jest/prefer-strict-equal': 'warn',
-        'jest/prefer-todo': 'warn',
-      },
-    },
-  ],
 };
