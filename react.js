972be4ee7a5c41e7c441f98f3a9523b4270137bc
Sergey Slipchenko <faergeek@gmail.com>
2024-02-13 07:19:41 +0400

    chore: remove react-classic config
    BREAKING CHANGE: react-classic config doesn't exist anymore

--- react.js
+++ react.js
@@ -1,7 +1,25 @@
 module.exports = {
-  extends: ['./react-classic'],
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  env: { 'shared-node-browser': true },
+  settings: { react: { version: 'detect' } },
   rules: {
+    'react/display-name': 'off',
+    'react/jsx-boolean-value': 'warn',
+    'react/jsx-curly-brace-presence': 'warn',
+    'react/jsx-fragments': 'warn',
+    'react/jsx-key': [
+      'warn',
+      { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true },
+    ],
+    'react/jsx-no-target-blank': [
+      'error',
+      { allowReferrer: true, warnOnSpreadAttributes: true },
+    ],
     'react/jsx-uses-react': 'off',
+    'react/no-danger': 'warn',
+    'react/no-unused-prop-types': 'warn',
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/require-render-return': 'off',
   },
 };
