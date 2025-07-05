497fe9ec4edac6ad9b939a6991aba4026c5ab61e
Sergei Slipchenko <faergeek@gmail.com>
2025-07-05 08:48:09 +0400

    chore: remove `@typescript-eslint/explicit-module-boundary-types`

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ export const typescript = defineConfig({
   rules: {
     '@typescript-eslint/array-type': ['warn', { default: 'array-simple' }],
     '@typescript-eslint/consistent-type-imports': 'warn',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     '@typescript-eslint/no-shadow': 'warn',
     '@typescript-eslint/no-unused-vars': 'off',
