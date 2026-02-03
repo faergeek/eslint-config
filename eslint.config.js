55ac8cf6fc0b5ea305b430ad6420d8dd941fcaff
Sergei Slipchenko <faergeek@gmail.com>
2026-02-03 08:52:43 +0400

    feat: publish types

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,7 @@
-export { base as default } from './index.js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import { base, typescript } from './index.js';
+
+export default defineConfig(globalIgnores(['dist']), {
+  extends: [base, typescript],
+});
